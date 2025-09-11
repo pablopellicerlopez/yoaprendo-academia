8eea60efed1ac97430bd82b83f028dedcd4e6515
pablopellicerr <154909548+pablopellicerlopez@users.noreply.github.com>
2025-09-11 20:24:00 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// Slider testimonios Swiper
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  autoplay: {
+    delay: 5000,
+  },
+});
